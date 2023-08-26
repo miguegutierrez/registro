33b0503b0ca499a5f84117f7c3113411fd2a4e24
miguegutierrez <miguelonmelon92@gmali.com>
2023-08-26 17:39:41 -0300

    primera correccion del codigo

--- js/validacion.js
+++ js/validacion.js
@@ -6,57 +6,28 @@ function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 };
 
-const form = document.getElementById('form');
-const listInput = document.querySelectorAll(".form-control");
-const myName = document.getElementById("nombre")
-const surName = document.getElementById("apellido");
-const email = document.getElementById("email");
-const password = document.getElementById("password1");
-const confirmPassword = document.getElementById("password2");
-const acceptTerms = document.getElementById("aceptoTerminos");
+  
+  function validateForm() {
+    const myName = document.getElementById('nombre').value;
+    const surName = document.getElementById('apellido').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password1').value;
+    const confirmPassword = document.getElementById('password2').value;
+    const acceptTerms = document.getElementById('terminos').checked;
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    listInput.forEach(element => {
-        element.lastElementChild.innerHTML = "";
-    });
-
-    if (myName.value.length < 1 || myName.value.trim() == "") {
+    if (myName === '' || surName === '' || email === '' || password === '' || confirmPassword === '' || !acceptTerms) {
       showAlertError();
-    }else{
-        showAlertSuccess()
+      return;
     }
-    if (surName.value.length < 1 || surName.value.trim() == "") {
-        showAlertError();
-      }else{
-        showAlertSuccess()
+
+    if (password.length < 6 && confirmPassword !== password) {
+      showAlertError();
+      return;
+    }else{
+
+        showAlertSuccess();
       }
-      if (email.value.length < 1 || email.value.trim() == "") {
-        showAlertError();
-      }else{
-        showAlertSuccess()
-      }
-      if (password.value.length < 6 || password.value.trim() == "") {
-        showAlertError();
-      } else{
-        showAlertSuccess()
-      }
-      if (confirmPassword.value !== password.value) {
-        showAlertError();
-      }else{
-        showAlertSuccess()
-      }
-      if(!acceptTerms.checked){
-        showAlertError();
-      }else{
-        showAlertSuccess()
-
-      }
-   
-});
-
-
-
-
-
+        }
+  // Asigna la función de validación al botón de registro
+  const regBtn = document.getElementById('regBtn');
+  regBtn.addEventListener('click', validateForm);
